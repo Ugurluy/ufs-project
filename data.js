0faf4643edff0714e091403a128ff48a3858f683
Yagiz Ugurlu <45629704+Ugurluy@users.noreply.github.com>
2019-12-19 18:58:53 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    jobId: 1,
+    companyName: "Haritalama Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/Fs5DpTsn/brennan-burling-248638-unsplash.jpg"
+  },
+  {
+    jobId: 2,
+    companyName: "Hook and Loop Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/Fs5DpTsn/brennan-burling-248638-unsplash.jpg"
+  },
+  {
+    jobId: 3,
+    companyName: "Yapıştırma Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  },
+  {
+    jobId: 4,
+    companyName: "Kesim Günlük İmalat Raporu",
+    img: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
+  },
+  {
+    jobId: 5,
+    companyName: "İz Basım Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  },
+  {
+    jobId: 6,
+    companyName: "Dikiş Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  },
+  {
+    jobId: 7,
+    companyName: "Iskarta Takip Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  },
+  {
+    jobId: 8,
+    companyName: "Kalite Kontrol Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  },
+  {
+    jobId: 9,
+    companyName: "Stok Ambalaj Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  },
+  {
+    jobId: 10,
+    companyName: "Ekli Keçe Günlük Parça Takip Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  },
+  {
+    jobId: 11,
+    companyName: "Kırpım - Ütü Günlük İmalat Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  },
+  {
+    jobId: 12,
+    companyName: "Desi - Parça Takip Raporu",
+    img: "https://i.postimg.cc/52b1rSwQ/pure-virtual-1302902-unsplash.jpg"
+  }
+];
---
+++ reportsData.js
@@ -0,0 +1,134 @@
+export default [
+    {
+      jobId: 1,
+      nameOfDoc: "Haritalama Günlük İmalat Raporu",
+      customer: "",
+      radius: "",
+      quantityOfOrder: null,
+      qualityOfSkin: null,
+      hairLength: null,
+      kindOfSkin: "",
+      kindOfVelcro:"",
+      colorOfVelcro:""
+    },
+    {
+      jobId: 2,
+      nameOfDoc: "Hook and Loop Günlük İmalat Raporu",
+      customer: "",
+      radius: "",
+      quantityOfOrder: null,
+      hairLength: null,
+      kindOfSkin: "",
+      kindOfVelcro:"",
+      colorOfVelcro:"",
+      kindOfSponge:"",
+      kindOfBackingPad:"",
+      quantityOfSkin: null,
+      quantityOfTemplate: null
+    },
+    {
+      jobId: 3,
+      nameOfDoc: "Yapıştırma Günlük İmalat Raporu",
+      customer: "Domestic",
+      radius: 160,
+      quantityOfOrder: null,
+      hairLength: null,
+      kindOfSkin: "",
+      kindOfVelcro:"",
+      colorOfVelcro:"",
+      kindOfSponge:"",
+      kindOfBackingPad:"",
+      kindOfAdhesive:"",
+      quantityOfSkin: null,
+      pieceQuant: null,
+      attachedPieceQuant: null,
+      tapeQuant: "Şerit Adet",
+      focusQuant: "Focus Adet"      
+    },
+    {
+      jobId: 4,
+      nameOfDoc: "Kesim Günlük İmalat Raporu",
+      customer: "Domestic",
+      radius: 160,
+      quantityOfOrder: "Kalın",
+      hairLength: null,
+      kindOfSkin: "",
+      kindOfVelcro:"",
+      colorOfVelcro:"",
+      quantOfSkinToCut:"Kesime Giren Deri Adet",
+      quantOfFrameCut: null,
+      pieceQuant: null,
+      totalQuantCut: null    
+    },
+    {
+      jobId: 5,
+      nameOfDoc: "İz Basım Günlük İmalat Raporu",
+      customer: "",
+      radius: 135,
+      quantityOfOrder: null,
+      hairLength: null,
+      kindOfSkin: "",
+      kindOfVelcro:"",
+      colorOfVelcro:"",
+      hotPressQuant: null,
+      laminaQuant: null,
+      focusCircleQuant: null      
+    },
+    {
+      jobId: 191086,
+      nameOfDoc: "Sumax",
+      radius: 135,
+      customer: "Merino",
+      quantityOfOrder: "Kalın"
+      
+    },
+    {
+      jobId: 191692,
+      nameOfDoc: "BPO",
+      radius: 80,
+      customer: "Merino",
+      quantityOfOrder: "Dijital"
+      
+    },
+    {
+      jobId: 191598,
+      nameOfDoc: "Scholl",
+      radius: 80,
+      customer: "Merino",
+      quantityOfOrder: "İnce"
+      
+    },
+    {
+      jobId: 191712,
+      nameOfDoc: "Befar",
+      radius: 150,
+      customer: "Domestic",
+      quantityOfOrder: "Kalın"
+      
+    },
+    {
+      jobId: 191712,
+      nameOfDoc: "Befar",
+      radius: 160,
+      customer: "Domestic",
+      quantityOfOrder: "Kalın"
+      
+    },
+    {
+      jobId: 191712,
+      nameOfDoc: "Befar",
+      radius: 180,
+      customer: "Domestic",
+      quantityOfOrder: "Kalın"
+      
+    },
+    {
+      jobId: 191693,
+      nameOfDoc: "BPO",
+      radius: 80,
+      customer: "Merino",
+      quantityOfOrder: "Dijital"
+      
+    },
+  ];
+  
